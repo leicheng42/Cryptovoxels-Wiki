23db4c46c8e827339e876654337c3dbfffed8a83
leicheng42 <leicheng42@gmail.com>
2023-08-02 17:25:39 +0800

    12

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+import sphinx_rtd_theme
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -25,5 +27,5 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 html_static_path = ['_static']
