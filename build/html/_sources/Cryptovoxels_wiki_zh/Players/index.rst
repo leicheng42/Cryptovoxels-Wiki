56a5270c58d29db101c432fea5fcafceae65c843
leicheng42 <leicheng42@gmail.com>
2023-08-02 20:36:11 +0800

    init1

---
+++ build/html/_sources/Cryptovoxels_wiki_zh/Players/index.rst
@@ -0,0 +1,24 @@
+.. Voxels Wiki documentation master file, created by
+   sphinx-quickstart on Wed Aug  2 16:15:29 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+玩家
+=======================================
+
+.. toctree::
+   :maxdepth: 1
+
+   1. Customization 自定义.md
+   2. Make a wearable 制作穿戴品.md
+   3. Buy a wearable 购买穿戴品.md
+   4. Usernames 用户名.md
+   5. Key bindings 关键绑定.md
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
--- docs/conf.py
+++ docs/conf.py
@@ -16,7 +16,11 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark']
+extensions = [
+    'myst_parser',
+    'sphinx.ext.duration',
+    'sphinx.ext.autosectionlabel',
+    ]
 source_suffix = {'.rst': 'restructuredtext', '.txt': 'markdown', '.md': 'markdown',}
 
 templates_path = ['_templates']
@@ -27,5 +31,5 @@ language = 'zh_CN'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx_book_theme"
 html_static_path = ['_static']
